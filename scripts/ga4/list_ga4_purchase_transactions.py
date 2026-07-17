bb61c54f6dc39c1225d3431c1ae2aa9300a4ed34
Mon711 <mrinalsood711@gmail.com>
2026-07-17 21:35:11 +0530

    change ga4 purchase transactions files' date range

--- scripts/ga4/list_ga4_purchase_transactions.py
+++ scripts/ga4/list_ga4_purchase_transactions.py
@@ -27,7 +27,12 @@ def main() -> None:
             Dimension(name="itemVariant"),
         ],
         metrics=[Metric(name="itemsPurchased"), Metric(name="itemRevenue")],
-        date_ranges=[DateRange(start_date="7daysAgo", end_date="today")],
+        date_ranges=[
+            DateRange(
+                start_date="2026-07-01",
+                end_date="2026-07-07",
+            )
+        ],
         dimension_filter=FilterExpression(
             filter=Filter(
                 field_name="eventName",
@@ -41,7 +46,7 @@ def main() -> None:
             )
         ],
     )
-    
+
     try:
         response = client.run_report(request)
     except GoogleAPICallError as error:
@@ -60,13 +65,13 @@ def main() -> None:
             value.value for value in row.dimension_values
         ]
         purchases, item_revenue = [value.value for value in row.metric_values]
-        
+
         # Format date nicely
         formatted_date = f"{date[:4]}-{date[4:6]}-{date[6:]}"
-        
+
         # Convert revenue string to a float so we can format it to 2 decimal places
         rev_float = float(item_revenue)
-        
+
         # 2. Data Row (Matching widths, right-aligned numbers with ':>')
         print(
             f"{formatted_date:<12} {transaction_id:<18} {item_name:<40} {item_id:<46} "
